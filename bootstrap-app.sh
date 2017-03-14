59b2ed5d573fd64a39b0ca1dbf5aa2515b5607ae
Brandon Egbert <brandonegbert@gmail.com>
2017-03-14 19:24:41 -0400

    Removed mysql install

--- bootstrap-app.sh
+++ bootstrap-app.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
-sudo apt-get -y install mysql-server
